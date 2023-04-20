7b73982a21e0314aad266b90d8a38ae1b736ebbb
Aidan Loughran <86127892+aidlran@users.noreply.github.com>
2023-04-20 23:16:42 +0100

    chore: vercel configuration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
   import { initServices } from '$lib/client';
   initServices();
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,12 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      regions: ['dub1'],
+      runtime: 'nodejs18.x',
+    }),
   },
 };
 
