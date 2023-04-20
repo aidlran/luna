247411fc5d036645f612c9cf3d46a08f90dba756
Aidan Loughran <86127892+aidlran@users.noreply.github.com>
2023-04-21 00:14:57 +0100

    chore(deps): update

--- vite.config.js
+++ vite.config.js
@@ -10,20 +10,6 @@ const config = {
       allow: [searchForWorkspaceRoot(process.cwd()), '../..'],
     },
   },
-  worker: {
-    plugins: [
-      // TODO: remove once vite 4.3 is out
-      {
-        name: 'remove-manifest',
-        configResolved(c) {
-          const manifestPlugin = c.worker.plugins.findIndex((p) => p.name === 'vite:manifest');
-          c.worker.plugins.splice(manifestPlugin, 1);
-          const ssrManifestPlugin = c.worker.plugins.findIndex((p) => p.name === 'vite:ssr-manifest');
-          c.plugins.splice(ssrManifestPlugin, 1);
-        },
-      },
-    ],
-  },
 };
 
 export default config;
