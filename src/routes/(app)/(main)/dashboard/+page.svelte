6a3941aa6cf99d7dc4bc1c204b12e56923262fef
Aidan Loughran <aidan@loughran.dev>
2023-07-27 20:55:26 +0100

    refactor: lib auto decrypts data

--- src/routes/(app)/(main)/dashboard/+page.svelte
+++ src/routes/(app)/(main)/dashboard/+page.svelte
@@ -3,28 +3,13 @@
   import { TaskList } from '$lib/client/components';
   import type { ITodo } from '$lib/client/interfaces/todo.interface';
 
-  async function getData(): Promise<ITodo[]> {
-    const allData = await Data.getAllOwn();
-    return (
-      await Promise.all(
-        allData.map(async (data) => {
-          const { id } = data;
-          try {
-            return {
-              id,
-              ...JSON.parse(await Data.decrypt(data)),
-            };
-          } catch (e) {
-            return null;
-          }
-        }),
-      )
-    ).filter((item) => item !== null) as ITodo[];
+  function getOwnTasks() {
+    return Data.getAllOwn() as Promise<ITodo[]>;
   }
 </script>
 
 <div class="task-list-container">
-  {#await getData() then items}
+  {#await getOwnTasks() then items}
     <TaskList listName="My Tasks" {items} />
   {/await}
 </div>
