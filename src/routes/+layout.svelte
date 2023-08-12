79e1d30de6ef8e0d880b609365240de9195b9bff
Aidan Loughran <aidan@loughran.dev>
2023-08-12 21:43:42 +0100

    chore: add svelte lib

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script>
   import '../styles.css';
-  import { inject } from '@vercel/analytics';
-  import { dev } from '$app/environment';
   import { initServices } from '$lib/client/utils/services';
 
   // Make services available for DI
   initServices();
-
-  // Inject @vercel/analytics
-  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
