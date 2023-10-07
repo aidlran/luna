a8aec63c7630f1ceab58fcc1041328f8ed709d57
Aidan Loughran <aidan@loughran.dev>
2023-10-07 20:25:38 +0100

    breaking: delete api routes

--- src/routes/api/user/+server.ts
+++
@@ -1,6 +0,0 @@
-import { userController } from '$lib/server/utils/context';
-import type { UserCreateDTO } from '$lib/shared/dtos';
-import type { RequestEvent } from './$types';
-
-export const POST = (requestEvent) =>
-  userController.post(requestEvent as RequestEvent & { request: { dto: UserCreateDTO } });
