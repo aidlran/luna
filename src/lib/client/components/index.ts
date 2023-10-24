6d3b26561d6e47f16a3ca0e876c227a3baa8e4e3
Aidan Loughran <aidan@loughran.dev>
2023-10-24 01:59:13 +0100

    feat(style): extended global styles

--- src/lib/client/components/index.ts
+++ src/lib/client/components/index.ts
@@ -1,21 +1,10 @@
 import LoadingSpinner from './animation/loading-spinner.svelte';
 import Tick from './animation/tick.svelte';
 
-import Authenticate from './authenticate.svelte';
-import Card from './card.svelte';
 import Header from './header.svelte';
 import Modal from './modal.svelte';
 import TaskList from './task-list.svelte';
 
 export * from './drawer';
 
-export {
-  /** @deprecated Kept for style reference, do not use. */
-  Authenticate,
-  Card,
-  Header,
-  LoadingSpinner,
-  Modal,
-  TaskList,
-  Tick,
-};
+export { Header, LoadingSpinner, Modal, TaskList, Tick };
