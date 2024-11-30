8b35b6fe23e6674a861df86f52fa0153e7d20954
grugbit <86127892+aidlran@users.noreply.github.com>
2024-11-30 18:11:56 +0000

    feat: add settings page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
   import { indexeddb } from '@astrobase/core/indexeddb';
   import { clients } from '@astrobase/core/rpc/client';
+  import { page } from '$app/stores';
   import '$lib/client/styles/main.css';
 
   let { children } = $props();
@@ -10,6 +11,12 @@
   <title>LUNA Project Management</title>
   <meta name="description" content="LUNA: productivity and project management system." />
 </svelte:head>
+
+<header class="border">
+  {#each [['/', 'Tasks'], ['/settings', 'Settings']] as [path, text]}
+    <a class="button" class:text-blue-700={$page.url.pathname === path} href={path}>{text}</a>
+  {/each}
+</header>
 
 {#await indexeddb().then((strategy) => clients.add({ strategy })) then}
   {@render children()}
