8134aa77774fb1f168d5c7510f596a8aeeefd4cd
Aidan <86127892+aidlran@users.noreply.github.com>
2025-09-05 15:00:50 +0100

    chore(projects): prune template project

--- apps/projects-solid/src/entry-client.tsx
+++ apps/projects-solid/src/entry-client.tsx
@@ -1,4 +1,5 @@
 // @refresh reload
-import { mount, StartClient } from "@solidjs/start/client";
+import { mount, StartClient } from '@solidjs/start/client';
 
-mount(() => <StartClient />, document.getElementById("app")!);
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+mount(() => <StartClient />, document.getElementById('app')!);
--- apps/projects-solid/src/routes/[...404].tsx
+++ apps/projects-solid/src/routes/[...404].tsx
@@ -1,19 +1,12 @@
-import { Title } from "@solidjs/meta";
-import { HttpStatusCode } from "@solidjs/start";
+import { Title } from '@solidjs/meta';
+import { HttpStatusCode } from '@solidjs/start';
 
-export default function NotFound() {
+export default function () {
   return (
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
       <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
     </main>
   );
 }
