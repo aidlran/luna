746ce1a78307d2a027d48eaddec0922776289211
Aidan <86127892+aidlran@users.noreply.github.com>
2025-09-10 15:23:08 +0100

    chore(projects): add indexeddb to base astrobase config

--- apps/projects-solid/astrobase.config.ts
+++ apps/projects-solid/astrobase.config.ts
@@ -1,8 +1,12 @@
 import { Common } from '@astrobase/sdk/common';
+import { indexeddb } from '@astrobase/sdk/indexeddb';
 import { createInstance } from '@astrobase/sdk/instance';
+import { createResource } from 'solid-js';
 
 /**
  * The base Astrobase config. The application derives a reactive final config value with user
  * configuration on top.
  */
-export default createInstance(Common);
+export default createResource(async () =>
+  createInstance(Common, { clients: [{ strategy: await indexeddb() }] }),
+)[0];
--- apps/projects-solid/src/lib/astrobase-merged-config.ts
+++ apps/projects-solid/src/lib/astrobase-merged-config.ts
@@ -2,4 +2,4 @@ import { createMemo } from 'solid-js';
 import baseConfig from '../../astrobase.config';
 
 /** The reactive merged Astrobase config. We'll add a source signal with the user's configuration. */
-export default createMemo(() => baseConfig);
+export default createMemo(() => baseConfig());
